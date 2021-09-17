e50b93cabe3cd3f64e512f812d26a476bb3ea673
carlos daniel <a20160383@gmail.com>
2021-09-16 21:41:09 -0500

    tests for the models and controller were added
    added tests for the model
    for the creation of the product by csv and for the rest of the models.

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Recorded if all the fields are valid.' do
+    buyer = FactoryBot.create(:buyer)
+    buyer.save
+    expect(Buyer.count).to eq(1)
+  end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
+require 'csv'
 
 RSpec.describe "Products", type: :request do
   describe "GET /products" do
     it "works! (now write some real specs)" do
       get products_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '.import' do
+    before :each do
+      @file = fixture_file_upload('files1.csv', 'text/csv')
+    end
+    it 'import' do
+      expect { post import_products_path, params: { file: @file } }.to change(Product, :count).by(5)
+      expect(response).to have_http_status(:found)
     end
   end
 end
